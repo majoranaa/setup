bb5b64331491559bc730ad31e0a9ade14c578ded
Kai Yang <kai.yang412@gmail.com>
2016-12-14 20:32:02 -0800

    fixed space typo in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "`uname`" == "Linux" ]; then # Linux OS
     ./setup_linux.sh
-elif ["`uname`" == "Darwin" ]; then # Mac OS
+elif [ "`uname`" == "Darwin" ]; then # Mac OS
     ./setup_mac.sh
 fi
